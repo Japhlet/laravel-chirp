84210197a85cd201f254ccb374a0316144dd288f
japhlet <etyangemodojaphlet@yahoo.com>
2025-02-13 13:34:48 +0300

    Implemented functionality to add chirps in the database

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
-Use Illuninate\RedirectResponse;
+Use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
